54c11cc17b586f0ce5f918666294796acda7f7ef
Kay Schroeder <katie@itskatie.com>
2025-05-23 13:34:26 -0700

    Restructuring Project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
     repositories {
         google()
-        mavenCentral()
         gradlePluginPortal()
+        mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "FSRS Android"
-include(":app")
- 
+rootProject.name = "kmp-fsrs"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/androidUnitTest/kotlin/com/example/kmp_fsrs/Test.android.kt
@@ -0,0 +1,12 @@
+package com.example.kmp_fsrs
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AndroidGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
+    }
+}
